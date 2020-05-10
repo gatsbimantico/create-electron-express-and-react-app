2796b5cdfb1ee59a7690409fa6bf9fa575515a5b
gatsbimantico <gatsbimantico@gmail.com>
2020-05-10 21:36:40 +0100

    Proven built also on windows

--- bump_versions.js
+++ bump_versions.js
@@ -3,7 +3,7 @@ const pkg = require('./package.json');
 const dependencies = Object.keys(pkg.dependencies).concat(Object.keys(pkg.devDependencies));
 
 const spawn = require('cross-spawn');
-const child = spawn('yarn', ['add', ...dependencies], { stdio: 'inherit' });
+const child = spawn('npm', ['i', ...dependencies], { stdio: 'inherit' });
 
 child.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
